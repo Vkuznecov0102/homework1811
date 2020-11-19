535ad0160b741c3ad9726d31f1e99947995a302e
Valeriy <simha88@yandex.ru>
2020-11-19 20:25:39 +0000

    Some changes and second day

--- src/main/java/arrayList/CustomArrayList.java
+++ src/main/java/arrayList/CustomArrayList.java
@@ -17,7 +17,7 @@ public class CustomArrayList {
         return realSize;
     }
 
-    public void add(Object o) {
+    public boolean add(Object o) {
         if (realSize == array.length) {
 
             Object[] resArray = new Object[array.length * 3 / 2 + 1];
@@ -26,6 +26,25 @@ public class CustomArrayList {
 
         }
         array[realSize++] = o;
+        return true;
+    }
+
+    public boolean remove(Object o) {
+        int delIndex = -1;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] != null && array[i].equals(o)) {
+                delIndex = i;
+                break;
+            }
+        }
+        if (array.length - 1 - delIndex >= 0)
+            System.arraycopy(array, delIndex + 1, array, delIndex, array.length - 1 - delIndex);
+        if (delIndex == -1) {
+            return false;
+        } else {
+            realSize--;
+            return true;
+        }
     }
 
 
@@ -35,18 +54,74 @@ public class CustomArrayList {
 
     public boolean contains(Object o) {
 
-        try {
-            for (Object value : array) {
-                if (value.equals(o)) return true;
-            }
-        } catch (NullPointerException npe) {
-
+        for (int i = 0; i < realSize; i++) {
+            if (array[i] != null && array[i].equals(o)) return true;
         }
+
         return false;
     }
 
     public void clear() {
-        Arrays.fill(array, null);
+        Arrays.fill(array, " ");
+    }
+
+    public Object remove(int index) {
+        checkIndex(index);
+        Object resElement = array[index];
+
+        if (array.length - 1 - index >= 0) {
+            System.arraycopy(array, index + 1, array, index, array.length - 1 - index);
+        }
+        realSize--;
+        return resElement;
+    }
+
+    private void checkIndex(int index) {
+        if (!isCorrectIndex(index)) {
+            throw new ArrayIndexOutOfBoundsException("Некорректен индекс");
+        }
+    }
+
+    private boolean isCorrectIndex(int index) {
+        return (index > -1) || (index < realSize);
+    }
+
+    public Object get(int index) {
+        return array[index];
+    }
+
+    public int indexOf(Object o) {
+        for (int i = 0; i < realSize; i++) {
+            if (array[i] == o) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public int lastIndexOf(Object o) {
+        for (int i = realSize - 1; i >= 0; i--)
+            if (array[i] == o)
+                return i;
+        return -1;
+    }
+
+    public Object set(int index, Object element) {
+        for (int i = 0; i < realSize; i++) {
+            if (i == index) {
+                array[i] = element;
+            }
+        }
+        return array;
+    }
+
+    public void add(int index, Object element) {
+
+        Object[] resArray = new Object[array.length + 1];
+        System.arraycopy(array, 0, resArray, 0, array.length);
+        System.arraycopy(array, index, resArray, index + 1, array.length - index - 1);
+        array[index] = element;
+
     }
 
     @Override
--- src/main/java/arrayList/Main.java
+++ src/main/java/arrayList/Main.java
@@ -11,14 +11,35 @@ public class Main {
         list.add("Это второй объект");
         list.add("Это третий объект");
         list.add("Это четвертый объект");
+        list.add("Это пятый объект");
+        list.add("Это шестой объект");
+        list.add("Это седьмой объект");
+        list.add("Это восьмой объект");
+        list.add("Это восьмой объект");
 
         System.out.println("list.size() = " + list.size());
         System.out.println("list.isEmpty() = " + list.isEmpty());
         System.out.println("list.contains(\"Это первый объект\") = " + list.contains("Это первый объект"));
 
-        list.clear();
         System.out.println(list);
 
+        System.out.println("list.get(2) = " + list.get(2));
 
+        System.out.println("list.indexOf(\"Это третий объект\") = " + list.indexOf("Это третий объект"));
+
+        System.out.println("list.lastIndexOf(\"Это восьмой объект\") = " + list.lastIndexOf("Это восьмой объект"));
+
+        list.remove("Это пятый объект");
+        list.remove(1);
+        System.out.println("После удаления объектов" + list);
+
+        list.set(0, "Это неизвестно откуда объект");
+        System.out.println(list);
+
+        list.add(4, "Объект из метода add");
+        System.out.println(list);
+
+        list.clear();
+        System.out.println(list);
     }
 }
